640a58d33dc6f1d1597fba8bae478a2ce1866b5d
zhourup <zhourup@163.com>
2022-03-22 22:57:42 +0800

    剑指 Offer 25. 合并两个排序的链表

---
+++ src/main/java/sword/Sword25.java
@@ -0,0 +1,51 @@
+package main.java.sword;
+
+/**
+ * @author zhourup
+ * @date 2022/3/22 22:56
+ */
+public class Sword25 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode curr = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        if (l1 != null) {
+            curr.next = l1;
+        }
+        if (l2 != null) {
+            curr.next = l2;
+        }
+        return dummyHead.next;
+    }
+}
