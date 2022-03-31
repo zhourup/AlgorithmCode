577479f52806367a369b5d542178f248903bb4fa
zhourup <zhourup@163.com>
2022-03-31 16:50:24 +0800

    剑指 Offer 55 - I. 二叉树的深度

---
+++ src/main/java/sword/Sword55I.java
@@ -0,0 +1,42 @@
+package main.java.sword;
+
+/**
+ * @author zhourup
+ * @date 2022/3/31 16:48
+ */
+public class Sword55I {
+
+    static private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 返回二叉树的深度
+     *
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+
+}
